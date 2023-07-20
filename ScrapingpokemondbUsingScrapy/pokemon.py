f0e7b82be0886ae2035a77ab5298f0e1b3282229
Muhammad Zohaib Afzaal <135415108+rajirajput23@users.noreply.github.com>
2023-07-20 12:24:59 +0500

    Add files via upload

--- ScrapingpokemondbUsingScrapy/pokemon.py
+++ ScrapingpokemondbUsingScrapy/pokemon.py
@@ -20,8 +20,43 @@ class PokemonSpider(scrapy.Spider):
             #print(table)
             #rows=response.css('table.data-table sticky-header block-wide ')
             #print(rows)
-            for row in response.css("table.data-table sticky-header block-wide  tr"):
+            for row in response.css("table.data-table sticky-header block-wide tr"):
+                 print(response)
                  Number=row.css("td.cell-num cell-fixed::text").get()
                  print(Number)
+                 Name=row.css("a.ent-name::text").get()
+                 print(Name)
+                 Type=row.css("td.cell-icon::text").get()
+                 print(Type)
+                 total=row.css("td.cell-num cell-total::text").get()
+                 print(total)
+                 HP=row.css("td.cell-num::text").get()
+                 print(HP)
+                 Attack=row.css("td.cell-num::text").get()
+                 print(Attack)
+                 Defence=row.css('td.cell-num::text').get()
+                 print(Defence)
+                 speed_atk=row.css('td.cell-num::text').get()
+                 print(speed_atk)
+                 speed_def=row.css('td.cell-num::text').get()
+                 print(speed_def)
+                 speed=row.css('td.cell-num::text').get()
+                 print(speed)
+
+                 yield{
+                      'Number':Number,
+                      'Name':Name,
+                      'Type':type,
+                      'total':total,
+                      'HP':HP,
+                      'Attack':Attack,
+                      'Defence':Defence,
+                      'speed_Atk':speed_atk,
+                      'speed_def':speed_def,
+                      'speed':speed
+                
+
+                 }
+
                      
        
